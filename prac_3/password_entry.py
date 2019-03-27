39aa085c3e045efc459939539510c403a76bf023
JarodDingwall <jarod.dingwall@my.jcu.edu.au>
2019-03-27 21:08:37 +1000

    Refactor password program to use functions

--- prac_3/password_entry.py
+++ prac_3/password_entry.py
@@ -4,12 +4,20 @@ MINIMUM_PASSWORD_LENGTH = 5
 
 
 def main():
-    password = input("Enter a password of at least {} characters: ".format(MINIMUM_PASSWORD_LENGTH))
+    password = get_password()
 
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    print("*" * len(password))
+
+
+def get_password():
+    password = input("Enter a password of at least {} characters: ".format(MINIMUM_PASSWORD_LENGTH))
     while len(password) < MINIMUM_PASSWORD_LENGTH:
         password = input("Enter a password of at least {} characters: ".format(MINIMUM_PASSWORD_LENGTH))
-
-    print("*" * len(password))
+    return password
 
 
 main()
