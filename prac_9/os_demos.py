5c03b781c2726678bc5439d696b8a3a1e235113c
JarodDingwall <jarod.dingwall@my.jcu.edu.au>
2019-05-21 22:50:23 +1000

    Add a loop that renames all files

--- prac_9/os_demos.py
+++ prac_9/os_demos.py
@@ -56,6 +56,10 @@ def demo_walk():
         print("(Current working directory is: {})".format(os.getcwd()))
 
         # TODO: add a loop to rename the files
+    for filename in filenames:
+        old_name = os.path.join(directory_name, filename)
+        new_name = os.path.join(directory_name, get_fixed_filename(filename))
+        os.rename(old_name, new_name)
 
 
 main()
